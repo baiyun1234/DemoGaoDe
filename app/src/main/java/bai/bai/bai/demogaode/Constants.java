27fa30ce16cf4065b95928313ff255a85f5c6143
baiyunyan <baiyunyan@wiseasy.com>
2019-01-29 17:32:12 +0800

    高德地图自定义Marker

---
+++ app/src/main/java/bai/bai/bai/demogaode/Constants.java
@@ -0,0 +1,12 @@
+package bai.bai.bai.demogaode;
+
+import com.amap.api.maps.model.LatLng;
+
+public class Constants {
+
+	public static final LatLng BEIJING = new LatLng(39.90403, 116.407525);// 北京市经纬度
+	public static final LatLng SHANGHAI = new LatLng(31.238068, 121.501654);// 上海市经纬度
+	public static final LatLng CHENGDU = new LatLng(30.679879, 104.064855);// 成都市经纬度
+	public static final LatLng XIAN = new LatLng(34.341568, 108.940174);// 西安市经纬度
+	public static final LatLng ZHENGZHOU = new LatLng(34.7466, 113.625367);// 郑州市经纬度
+}
